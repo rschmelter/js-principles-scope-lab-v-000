ed66684a46adccf93952d9f1840616c02585fd6d
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-02 23:59:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = "bob";
 
-function uppserCaseCustomerName() {
+function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
